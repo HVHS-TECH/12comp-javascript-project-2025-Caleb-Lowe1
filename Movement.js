c69176c7d40018d83bc2ddf177e66122b8fba27a
Caleb-Lowe1 <22026cl@hvhs.school.nz>
2025-03-17 23:36:31 +0000

    test5

--- Movement.js
+++ Movement.js
@@ -15,7 +15,7 @@ else if (kb.pressing('s')) {
     Player.vel.y = MOVEMENTSPEED;
 }
 //jump code
-if (kb.pressing('w') && Player.vel.y == 0 && Player.colliding(rock) || Player.colliding(cobblestone)) {
+if (kb.pressing('w') && Player.vel.y == 0 && Player.colliding(rock) || Player.colliding(cobblestone) && kb.pressing('w') && Player.vel.y == 0) {
     Player.vel.y = -JUMPSPEED;
     jumps = jumps + 1
     console.log(jumps);

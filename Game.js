54193ebd947ad10eda6ef6bf67de464f25c36715
Caleb-Lowe1 <22026cl@hvhs.school.nz>
2025-04-09 21:31:22 +0000

    codesaved

--- Game.js
+++ Game.js
@@ -14,17 +14,17 @@ health = 3;
 canvasSize = {
 	x: 1200,
 	y: 600
-	}
+}
 
 
-	let caveBg;
+let caveBg;
 
-	function preload() {
-	  // your other assets
-	   // replace with your file name
-	}
-var restartButton;	
-var backButton;	
+function preload() {
+	// your other assets
+	// replace with your file name
+}
+var restartButton;
+var backButton;
 var finish;
 //enviroment
 let sheetImg;
@@ -46,130 +46,131 @@ function preload() {
 /*******************************************************/
 function setup() {
 	gameState = "play";
-console.log("setup: ");
-cnv = new Canvas(canvasSize.x,canvasSize.y, "pixelated x4")
-world.gravity.y = 10;
+	console.log("setup: ");
+	cnv = new Canvas(canvasSize.x, canvasSize.y, "pixelated x4")
+	world.gravity.y = 10;
 
-finish = new Sprite (2002, 190);
-finish.spriteSheet = finishImg;
-finish.addAni ({w:16, h:16, row:0, col:0,}); 
-finish.collider = "none";	
-finish.scale = 4;
+	finish = new Sprite(2002, 190);
+	finish.spriteSheet = finishImg;
+	finish.addAni({ w: 16, h: 16, row: 0, col: 0, });
+	finish.collider = "none";
+	finish.scale = 4;
 
-Player = new Sprite(30, 178.01034375, 10, 10, 'd');
+	Player = new Sprite(30, 178.01034375, 10, 10, 'd');
 	Player.color = 'blue';
 	Player.stroke = 'black';
 	Player.strokeWeight = 1;
 
 
-lava = new Group();
-lava.collider = "static";
-lava.spriteSheet = sheetImg;
-lava.addAni({w:16, h:16, row:9, col:11 });
-lava.tile = 'l';
+	lava = new Group();
+	lava.collider = "static";
+	lava.spriteSheet = sheetImg;
+	lava.addAni({ w: 16, h: 16, row: 9, col: 11 });
+	lava.tile = 'l';
 
-hotrock = new Group();
-hotrock.collider = "static";
-hotrock.spriteSheet = sheetImg;
-hotrock.addAni({w:16, h:16, row:9, col:0 });
-hotrock.tile = 'h';
+	hotrock = new Group();
+	hotrock.collider = "static";
+	hotrock.spriteSheet = sheetImg;
+	hotrock.addAni({ w: 16, h: 16, row: 9, col: 0 });
+	hotrock.tile = 'h';
 
 
-emerald = new Group();
-emerald.collider = "none";
-emerald.spriteSheet = emeraldImg;
-emerald.addAni({w:16, h:16, row:0, col:0 });
-emerald.tile = 'e';
+	emerald = new Group();
+	emerald.collider = "none";
+	emerald.spriteSheet = emeraldImg;
+	emerald.addAni({ w: 16, h: 16, row: 0, col: 0 });
+	emerald.tile = 'e';
 
-diamond = new Group();
-diamond.collider = "none";
-diamond.spriteSheet = diamondImg;
-diamond.addAni({w:16, h:16, row:0, col:0 });
-diamond.tile = 'd';
+	diamond = new Group();
+	diamond.collider = "none";
+	diamond.spriteSheet = diamondImg;
+	diamond.addAni({ w: 16, h: 16, row: 0, col: 0 });
+	diamond.tile = 'd';
 
-rock = new Group();
-rock.collider = "static";
-rock.spriteSheet = sheetImg;
-rock.addAni({w:16, h:16, row:0, col:7 });
-rock.tile = 'r';
+	rock = new Group();
+	rock.collider = "static";
+	rock.spriteSheet = sheetImg;
+	rock.addAni({ w: 16, h: 16, row: 0, col: 7 });
+	rock.tile = 'r';
 
-cobblestone = new Group();
-cobblestone.collider = "static";
-cobblestone.spriteSheet = sheetImg;
-cobblestone.addAni({w:16, h:16, row:30, col:10 });
-cobblestone.tile = 'c';
+	cobblestone = new Group();
+	cobblestone.collider = "static";
+	cobblestone.spriteSheet = sheetImg;
+	cobblestone.addAni({ w: 16, h: 16, row: 30, col: 10 });
+	cobblestone.tile = 'c';
 
-unclimableblock = new Group();
-unclimableblock.collider = "static";
-unclimableblock.spriteSheet = sheetImg;
-unclimableblock.addAni({w:16, h:16, row:1, col:9 });
-unclimableblock.tile = 'w';
-unclimableblock.friction = 0;
+	unclimableblock = new Group();
+	unclimableblock.collider = "static";
+	unclimableblock.spriteSheet = sheetImg;
+	unclimableblock.addAni({ w: 16, h: 16, row: 1, col: 9 });
+	unclimableblock.tile = 'w';
+	unclimableblock.friction = 0;
 
-new Tiles([
-'rrrrrcrrrrrrrrrrrcrrrrrrrrrrcrrrrrrrrrrrrrrcccccrrrrrrrcrrrrrrcrrrrrrrrrrrrrrrrrcrrrrrrcrrrrrrrrrrrrrcrrrrrrrrrrcccrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr',
-'w.......',
-'w...................................c....................................................c',
-'w...................................rr...................................................rr',
-'w..........................rrcrrrcccrcr.........................................rrcrrrcccrcr',
-'w...................................rr.........e.........................................rr',
-'w...................................c....................................................c',
-'w.......................e',	
-'w......................hrc.......................e',
-'w........d........rr.........................r....................r............................................e',
-'w............................................r....................r....................................r.......',
-'w...........cr...................d...........r....................c....................................',
-'rrrrrcrrr.......................................................rrr..................d...........r',
-'rrrrrhrrrrr......d.....................................d........hcrr......d............................................d',
-'rcrccccrrrrrrrrrrrrrrrhhhrrrrrcr..rrrrhrrrrrrrrrcccllcccrrrchrcrrrrcrrrrrrrcrrrcrrr....rrrhhrrrcrrrrcrrcrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr',
-'rchhrrrrhrrrcrrrrcrrrrrrrrrrrrrw..wrrrhrrrrrrhrrrrrrrrrrrrrrrrrrrrrrhrr...........w....w',
-'rrcrrrrrrrrrrrrrrrrrcrhrrrrrrrrhllhrrrrrrhrhhrrccrrrrrhrrcrrrrrrcrrrrrr...........wllllw',
-'rrrrrrrrrcrrrrccrrrrrrrrcrrrrrrhhhhrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr...........wwwwww'
-],
-0, 0, //x,y
-16,16, //w,h 
+	new Tiles([
+		'rrrrrcrrrrrrrrrrrcrrrrrrrrrrcrrrrrrrrrrrrrrcccccrrrrrrrcrrrrrrcrrrrrrrrrrrrrrrrrcrrrrrrcrrrrrrrrrrrrrcrrrrrrrrrrcccrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr',
+		'w.......',
+		'w...................................c....................................................c',
+		'w...................................rr...................................................rr',
+		'w..........................rrcrrrcccrcr.........................................rrcrrrcccrcr',
+		'w...................................rr.........e.........................................rr',
+		'w...................................c....................................................c',
+		'w.......................e',
+		'w......................hrc.......................e',
+		'w........d........rr.........................r....................r............................................e',
+		'w............................................r....................r....................................r.......',
+		'w...........cr...................d...........r....................c....................................',
+		'rrrrrcrrr.......................................................rrr..................d...........r',
+		'rrrrrhrrrrr......d.....................................d........hcrr......d............................................d',
+		'rcrccccrrrrrrrrhrrrrrrhhhrrrrrcr..rrrrhrrrrrrrrrcccllcccrrrchrcrrrrcrrrrrrrcrrrcrrr....rrrhhrrrcrrrrcrrcrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr',
+		'rchhrrrrhrrrcrrrrcrrrrrrrrrrrrrw..wrrrhrrrrrrhrrrrrrrrrrrrrrrrrrrrrrhrr...........w....w',
+		'rrcrrrrrrrrrrrrrrrrrcrhrrrrrrrrhllhrrrrrrhrhhrrccrrrrrhrrcrrrrrrcrrrrrr...........wllllw',
+		'rrrrrrrrrcrrrrccrrrrrrrrcrrrrrrhhhhrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr...........wwwwww'
+	],
+		0, 0, //x,y
+		16, 16, //w,h 
 
-)}
+	)
+}
 
 //if the player collects a diamond the diamond will disapear and the user will gain 100 score 
 function playercollectsdiamond(d) {
-    
-    d.remove();
-    score = score + 70;
 
-    
-	
+	d.remove();
+	score = score + 70;
+
+
+
 }
 //if player collects an emerald they will earn 120 score points and the emerald will disapear
 function playercollectsemerald(e) {
-    
-    e.remove();
-    score = score + 120;
 
-    
-	
+	e.remove();
+	score = score + 120;
+
+
+
 }
 
 
 
 
-function win() {console.log("you win")}
+function win() { console.log("you win") }
 /*******************************************************/
 // draw()
 /*******************************************************/
 function draw() {
 
 	if (gameState == "play") {
-        runGame();
-		
-    }
-	
-    if (gameState == "win") {
-        win();
-    }
-    if (gameState == "lose") {
-        lose();
-    };
+		runGame();
+
+	}
+
+	if (gameState == "win") {
+		win();
+	}
+	if (gameState == "lose") {
+		lose();
+	};
 
 }
 
@@ -182,111 +183,112 @@ function draw() {
 function displayScore() {
 	textSize(20);
 	fill(500);
-	
-	text("Score: " + score, 0, 15);	
-	
+
+	text("Score: " + score, 0, 15);
+
 }
 
-function runGame(){
-	
-	
- 
-clear();
-
-background(caveBg1);
-background(caveBg2);
-background(caveBg3);
-background(caveBg4);
-healthbar();
-//makes the camera follow the player 
-camera.x = Player.x;
-camera.y = Player.y;	
-Movement();
-displayScore();
-Player.rotationLock = true;
+function runGame() {
 
 
+
+	clear();
+
+	background(caveBg1);
+	background(caveBg2);
+	background(caveBg3);
+	background(caveBg4);
+	healthbar();
+	//makes the camera follow the player 
+	camera.x = Player.x;
+	camera.y = Player.y;
+	Movement();
+	displayScore();
+	Player.rotationLock = true;
 
 
 
 
-//checking if the player has won
-if (Player.overlaps(finish)) {completedlevel();}
 
 
-//checking if the player has lost
-//Player.y >= 1300 is for if the player somehow managed to glitch through the map
-if (Player.y >= 1300 || (health <= 0)) 
-	{lostgame();}
-
-if (Player.collides (lava) || Player.collides (hotrock)) {Player.vel.y = -5; Player.vel.x = 1; health = (health - 1);
-	console.log(health)
+	//checking if the player has won
+	if (Player.overlaps(finish)) {
+		completedlevel();
 	}
-	
 
-if (diamond.overlaps(Player, playercollectsdiamond)) {
-	
-	playercollectsdiamond();
-		
+
+	//checking if the player has lost
+	//Player.y >= 1300 is for just in case the player somehow managed to glitch through the map
+	if (Player.y >= 1300 || (health <= 0)) {
+		lostgame();
+	}
+
+	if (Player.collides(lava) || Player.collides(hotrock)) {
+		Player.vel.y = -5; Player.vel.x = 1; health = (health - 1);
+	}
+
+
+	if (diamond.overlaps(Player, playercollectsdiamond)) {
+
+		playercollectsdiamond();
+
 	}
 
 
 	if (emerald.overlaps(Player, playercollectsemerald)) {
-	
+
 		playercollectsemerald();
-			
-		}	
-		
-	}		
-		
+
+	}
+
+}
 
 
-	function lose () {
-		console.log ("I LOST :(")
-		mouseInteractRestartButton();
-		mouseInteractBackButton();
-	
-	};
-	function lostgame()	{
-		gameState = "lose";
-		
-		Player.remove();
-		unclimableblock.removeAll();
-		diamond.removeAll();
-		cobblestone.removeAll();
-		rock.removeAll();
-		lava.removeAll();
-		emerald.removeAll();
-		hotrock.removeAll();
-		camera.x = canvasSize.x/2;
-		camera.y = canvasSize.y/2;
-		background("red");
-		textSize(20);
-		textAlign(CENTER, CENTER);
-		text("you lost!!", canvasSize.x/2, 50);
-		text("Score: "+ score, canvasSize.x/2, 100);
-		
-		
-		Restart();
-		
-		
-		Back();
 
-		
+function lose() {
+	console.log("I LOST :(")
+	mouseInteractRestartButton();
+	mouseInteractBackButton();
 
-		};
+};
+function lostgame() {
+	gameState = "lose";
+	Player.remove();
+	unclimableblock.removeAll();
+	diamond.removeAll();
+	cobblestone.removeAll();
+	rock.removeAll();
+	lava.removeAll();
+	emerald.removeAll();
+	hotrock.removeAll();
+	camera.x = canvasSize.x / 2;
+	camera.y = canvasSize.y / 2;
+	background("red");
+	textSize(20);
+	textAlign(CENTER, CENTER);
+	text("you lost!!", canvasSize.x / 2, 50);
+	text("Score: " + score, canvasSize.x / 2, 100);
 
-function win () {
+
+	Restart();
+
+
+	Back();
+
+};
+
+function win() {
 	console.log("WINNING")
-		mouseInteractRestartButton();
-		mouseInteractBackButton();
-	};
-function completedlevel(){
+	mouseInteractRestartButton();
+	mouseInteractBackButton();
+};
+function completedlevel() {
 	gameState = "win";
 	if (health == 3) {
-	score = score + 200;}
-	else if (health == 2) {score = score + 100;}
-	else if (health == 1) {score = score + 50;}
+		score = score + 200;
+	}
+	else if (health == 2) { score = score + 100; }
+	else if (health == 1) { score = score + 50; }
 	Player.remove();
 	unclimableblock.removeAll();
 	diamond.removeAll();
@@ -294,15 +296,15 @@ function completedlevel(){
 	cobblestone.removeAll();
 	rock.removeAll();
 	lava.removeAll();
-    hotrock.removeAll();
+	hotrock.removeAll();
 	camera.x = canvasSize.x / 2;
-    camera.y = canvasSize.y / 2;
+	camera.y = canvasSize.y / 2;
 	background("yellow");
 	fill(0)
 	textSize(20);
 	textAlign(CENTER, CENTER);
-	text("YOU WON!!", canvasSize.x/2, 50);
-	text("Score: "+ score, canvasSize.x/2, 100);
+	text("YOU WON!!", canvasSize.x / 2, 50);
+	text("Score: " + score, canvasSize.x / 2, 100);
 
 
 	Restart();
@@ -314,70 +316,72 @@ function completedlevel(){
 
 
 
-	
-function mouseInteractRestartButton () {
-    if (restartButton.mouse.hovering()) {
-        restartButton.addAni ({w:16, h:16, row:1, col:0,}); 
-	
-    }
 
-	else {
-        restartButton.addAni ({w:16, h:16, row:0, col:0,});     
-    }
-    if (restartButton.mouse.pressing()) {
-        window.location.href = "Game.html";
-    }}
-
-	function Restart(){
-		
-	restartButton = new Sprite(canvasSize.x / 2 - (32), canvasSize.y / 2);
-	  restartButton.spriteSheet = buttonImg;
-	  restartButton.addAni ({w:16, h:16, row:0, col:0,}); 
-	   restartButton.collider = "static";
-	restartButton.scale = 2;
-   };
-
-	function Back(){
-		backButton = new Sprite (canvasSize.x / 2 + (32), canvasSize.y / 2);
-		backButton.spriteSheet = backImg;
-		backButton.addAni ({w:16, h:16, row:1, col:0,}); 
-		backButton.collider = "static";	
-		backButton.scale = 2;
-		console.log("back working")
-	
+function mouseInteractRestartButton() {
+	if (restartButton.mouse.hovering()) {
+		restartButton.addAni({ w: 16, h: 16, row: 1, col: 0, });
 
 	}
-	
 
-	function mouseInteractBackButton () {
-		if (backButton.mouse.hovering()) {
-			backButton.addAni ({w:16, h:16, row:0, col:0,}); 
-			
-		
-		}
+	else {
+		restartButton.addAni({ w: 16, h: 16, row: 0, col: 0, });
+	}
+	if (restartButton.mouse.pressing()) {
+		window.location.href = "Game.html";
+	}
+}
+
+function Restart() {
+
+	restartButton = new Sprite(canvasSize.x / 2 - (32), canvasSize.y / 2);
+	restartButton.spriteSheet = buttonImg;
+	restartButton.addAni({ w: 16, h: 16, row: 0, col: 0, });
+	restartButton.collider = "static";
+	restartButton.scale = 2;
+};
+
+function Back() {
+	backButton = new Sprite(canvasSize.x / 2 + (32), canvasSize.y / 2);
+	backButton.spriteSheet = backImg;
+	backButton.addAni({ w: 16, h: 16, row: 1, col: 0, });
+	backButton.collider = "static";
+	backButton.scale = 2;
+	console.log("back working")
 
 
-	
-		else {
-			backButton.addAni ({w:16, h:16, row:1, col:0,});     
-		}
-		if (backButton.mouse.pressing()) {
-			window.location.href = "index.html";
-		}}
+}
 
-		function healthbar() {
-			fill(255); 
-			textSize(20); 
-			text("Health:", 10, 55);
-			for (var i = 0; i < health; i++) {
-				
-				fill(255, 0, 0);
-    			noStroke();
-    			rect(90 + 30 * i, 40, 20, 20); 
-				
-				
-			  }
-		}
+
+function mouseInteractBackButton() {
+	if (backButton.mouse.hovering()) {
+		backButton.addAni({ w: 16, h: 16, row: 0, col: 0, });
+
+
+	}
+
+
+
+	else {
+		backButton.addAni({ w: 16, h: 16, row: 1, col: 0, });
+	}
+	if (backButton.mouse.pressing()) {
+		window.location.href = "index.html";
+	}
+}
+
+function healthbar() {
+	fill(255);
+	textSize(20);
+	text("Health:", 10, 55);
+	for (var i = 0; i < health; i++) {
+
+		fill(255, 0, 0);
+		noStroke();
+		rect(90 + 30 * i, 40, 20, 20);
+
+
+	}
+}
 
 /*******************************************************/
 //  END OF APP
--- Movement.js
+++ Movement.js
@@ -1,33 +1,32 @@
 const MOVEMENTSPEED = 2.4;
 const JUMPSPEED = 4;
-function Movement() 
-{
-//movement code    
-if (kb.pressing('a') && kb.pressing('shift')) {
-    Player.vel.x = -MOVEMENTSPEED - 1;
-   
-}
-else if (kb.pressing('a')) {
-    Player.vel.x = -MOVEMENTSPEED + 0.5;
-   }
+function Movement() {
+    //movement code    
+    if (kb.pressing('a') && kb.pressing('shift')) {
+        Player.vel.x = -MOVEMENTSPEED - 1;
 
-if (kb.pressing('d') && kb.pressing('shift')) {
+    }
+    else if (kb.pressing('a')) {
+        Player.vel.x = -MOVEMENTSPEED + 0.5;
+    }
+
+    if (kb.pressing('d') && kb.pressing('shift')) {
         Player.vel.x = MOVEMENTSPEED + 1;
-     
-    }  
- else if (kb.pressing('d')) {
-    Player.vel.x = MOVEMENTSPEED - 0.5;
 
-} 
+    }
+    else if (kb.pressing('d')) {
+        Player.vel.x = MOVEMENTSPEED - 0.5;
 
- 
+    }
 
-//jump code, checks if the user is colliding with the ground and if true then and the user is pressing down on 'w' then it will allow the player to jump 
-if (kb.presses('w') && Player.colliding(rock) || kb.presses('w') && Player.colliding(cobblestone)) {
-    Player.vel.y = -(JUMPSPEED);
- 
-    } 
-  
+
+
+    //jump code, checks if the user is colliding with the ground and if true then and the user is pressing down on 'w' then it will allow the player to jump 
+    if (kb.presses('w') && Player.colliding(rock) || kb.presses('w') && Player.colliding(cobblestone)) {
+        Player.vel.y = -(JUMPSPEED);
+
+    }
+
 
 }
 
